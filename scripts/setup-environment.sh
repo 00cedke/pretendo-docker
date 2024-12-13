15276de77beb4b33cc0449fbcf89882fdd43a101
00cedke <mathiskera2509@gmail.com>
2024-12-13 18:23:48 +0100

    Add Mario Krart 8 Authentication Server.

--- scripts/setup-environment.sh
+++ scripts/setup-environment.sh
@@ -87,6 +87,7 @@ echo "PN_SMM_ACCOUNT_GRPC_API_KEY=$account_grpc_api_key" >>./super-mario-maker.l
 echo "PN_SPLATOON_ACCOUNT_GRPC_API_KEY=$account_grpc_api_key" >>./splatoon.local.env
 echo "PN_MINECRAFT_ACCOUNT_GRPC_API_KEY=$account_grpc_api_key" >>./minecraft-wiiu.local.env
 echo "PN_PIKMIN3_ACCOUNT_GRPC_API_KEY=$account_grpc_api_key" >>./pikmin-3.local.env
+echo "PN_MK8_ACCOUNT_GRPC_API_KEY=$account_grpc_api_key" >>./mario-kart-8.local.env
 
 # Generate a secret key for MinIO
 minio_secret_key=$(generate_password 32)
@@ -104,6 +105,7 @@ echo "PN_FRIENDS_CONFIG_DATABASE_URI=postgres://postgres_pretendo:$postgres_pass
 echo "PN_SMM_POSTGRES_URI=postgres://postgres_pretendo:$postgres_password@postgres/super_mario_maker?sslmode=disable" >>./super-mario-maker.local.env
 echo "PN_PIKMIN3_POSTGRES_URI=postgres://postgres_pretendo:$postgres_password@postgres/pikmin3?sslmode=disable" >>./pikmin-3.local.env
 echo "PN_SPLATOON_POSTGRES_URI=postgres://postgres_pretendo:$postgres_password@postgres/splatoon?sslmode=disable" >>./splatoon.local.env
+echo "PN_MK8_POSTGRES_URI=postgres://postgres_pretendo:$postgres_password@postgres/mk8.sslmode=disable" >>./mario-kart-8.local.env
 
 # Generate passwords, a gRPC API key, and an AES key for the friends server
 friends_authentication_password=$(generate_password 32)
@@ -131,12 +133,17 @@ echo "PN_SMM_KERBEROS_PASSWORD=$smm_kerberos_password" >>./super-mario-maker.loc
 splat_kerberos_password=$(generate_password 32)
 echo "PN_SPLATOON_KERBEROS_PASSWORD=$splat_kerberos_password" >>./splatoon.local.env
 
+# Generate a Kerberos password for the Minecraft Server.
 minecraft_kerberos_password=$(generate_password 32)
 echo "PN_MINECRAFT_KERBEROS_PASSWORD=$minecraft_kerberos_password" >>./minecraft-wiiu.local.env
 
 # Generate a Kerberos password for the Pikmin 3 server
 pikmin3_kerberos_password=$(generate_password 32)
 echo "PN_PIKMIN3_KERBEROS_PASSWORD=$pikmin3_kerberos_password" >>./pikmin-3.local.env
+
+# Generate a Kerberos password for the Mario Kart 8 Server.
+mk8_kerberos_password=$(generate_password 32)
+echo "PN_MK8_KERBEROS_PASSWORD=$mk8_kerberos_password" >>./pikmin-3.local.env
 
 # Generate an AES key for the Miiverse servers
 miiverse_aes_key=$(generate_hex 64)
@@ -156,6 +163,7 @@ echo "PN_SMM_SECURE_SERVER_HOST=$server_ip" >>./super-mario-maker.local.env
 echo "PN_SPLATOON_SECURE_SERVER_HOST=$server_ip" >>./splatoon.local.env
 echo "PN_MINECRAFT_SECURE_SERVER_HOST=$server_ip" >>./minecraft-wiiu.local.env
 echo "PN_PIKMIN3_SECURE_SERVER_HOST=$server_ip" >>./pikmin-3.local.env
+echo "PN_MK8_SECURE_SERVER_HOST=$server_ip" >>./mario-kart-8.local.env
 
 # Get the Wii U IP address
 if [[ -n "$wiiu_ip" ]]; then
